353d3717bc698bc22dd9d1255f5f8d8f1122315d
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-06-11 16:16:50 +0300

    PostManager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.manager;
+
+import ru.netology.domain.VKpagePost;
+
+public class PostManager {
+    private VKpagePost[] posts;
+
+    public VKpagePost[] getPosts() {
+        return posts;
+    }
+}
+
