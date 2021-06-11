3411acccef9a4bb199808e06ac86c26368a93934
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-06-11 13:34:45 +0300

    classes created

---
+++ src/main/java/ru/netology/domain/Comments.java
@@ -0,0 +1,11 @@
+package ru.netology.domain;
+
+public class Comments {
+    private int authorId;
+    private boolean canPost;
+    private boolean canCreatePhoto;
+    private boolean canAddFile;
+    private String text;
+
+//   get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/VKpagePost.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class VKpagePost {
+    private int id;
+    private String postName;
+    private int postDate;
+    private String postBody;
+    private String imageUrl;
+    private int likes;
+    private int wiews;
+    private int share;
+    private Comments comments;
+
+    //   get/set на все поля
+
+}
