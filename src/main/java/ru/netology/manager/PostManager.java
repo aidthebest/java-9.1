9a8e1f00a9cbc18220063ad2b492ea1fccf5595c
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-06-11 18:57:51 +0300

    add manager 2.0

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -3,10 +3,17 @@ package ru.netology.manager;
 import ru.netology.domain.VKpagePost;
 
 public class PostManager {
-    private VKpagePost[] posts;
+    private VKpagePost posts;
 
-    public VKpagePost[] getPosts() {
+    public VKpagePost getPosts() {
         return posts;
     }
-}
+    public int deliteWall (int ownerId,  int positiveId) {
+        return 0;
+    }
 
+    public String searchWall (int ownerId, String domain, String query, boolean ownersOnly,
+                              short count, short offset) {
+        return null;
+    }
+}
